5c39f868ee8eb2ae318d095e79f57f1475e049a0
James Kraai <james@jameskraai.com>
2015-11-14 22:27:42 -0600

    add rules

---
+++ zshHistory.plugin.zsh
@@ -0,0 +1,12 @@
+HISTFILE=$HOME/.zsh_history
+HIST_STAMPS="yyyy-mm-dd"
+HISTSIZE=1024
+SAVEHIST=1024
+setopt append_history
+setopt hist_ignore_all_dups
+unsetopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
+setopt bang_hist
